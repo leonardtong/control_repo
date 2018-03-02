42dcef99e7521c740a826b48c2c648bb4da2fdc2
leonardtong <36964997+leonardtong@users.noreply.github.com>
2018-03-01 16:12:02 -0800

    Update ssh_server.pp

--- site/profile/manifests/ssh_server.pp
+++ site/profile/manifests/ssh_server.pp
@@ -10,6 +10,6 @@ class profile::ssh_server {
 		ensure => present,
 		user   => 'root',
 		type   => 'ssh-rsa',
-		key    => '',
+		key    => 'AAAAB3NzaC1yc2EAAAADAQABAAABAQC2BPL1gQhfk/T/nzf7i9M+mQw+wlxfSmKZTJ2Tel13ZU5u1h+ilxm/5oBe1v/b+RG9vIRUEONK3AZu27mLcHPhreYZZpWRBbuDTMpei5cIe4AXk6rOl91mCsdb7CzmbLIlqNb0jNIfnOFietkqDUj7NyzYlIToTc4mWKw3FWs1jlPNrSApHDM9sNpLAkDDDzv/Oy1sEJmf4ubE7x2ksLKzFOEqYckPYDG6EKdVQPVM9xwHwYXEXdbnK5kxSfxpOOkIhhMrGhmo4zriJOqkcjDrDhtXL6n5guvx8SWhdVN6lJYF123wKC4x7ude0HCHMJZdeJLwh2R0Bb9cEqyOawFl',
 	}  
 }
